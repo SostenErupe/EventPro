c49e06b59a71ab003c09d3229a65a4edf35d8c55
SostenErupe <sostenekai@gmail.com>
2025-07-31 11:02:44 +0300

    initial commit

--- backend/db.js
+++ backend/db.js
@@ -3,7 +3,7 @@ const mysql = require('mysql2');
 const db = mysql.createConnection({
   host: 'localhost',
   user: 'root',
-  password: '',
+  password: 'Sosnigger@10',
   database: 'EventsInfo',
 });
 
