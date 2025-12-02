5c049dbe66cf9abbd1ca62f2b1529e61e79f1437
kileldylan <kileldylan@gmail.com>
2025-12-02 13:00:49 +0300

    new branch having the django backend

---
+++ django_backend/backend/models.py
@@ -0,0 +1,100 @@
+from django.db import models
+
+# Create your models here.
+class CustomUser(models.Model):
+    user_id = models.AutoField(primary_key=True)
+    username = models.CharField(max_length=100, unique=True)
+    email = models.CharField(max_length=10, unique=True)
+    password = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.username
+    
+class Venue(models.Model):
+    venue_id = models.AutoField(primary_key=True)
+    venue_name = models.CharField(max_length=100)
+    capacity = models.IntegerField()
+    street = models.CharField(max_length=100)
+    city = models.CharField(max_length=100)
+    state = models.CharField(max_length=100)
+    pincode = models.IntegerField()
+    
+class Event(models.Model):
+    event_id = models.AutoField(primary_key=True)
+    event_name = models.CharField(max_length=100)
+    event_date = models.DateField()
+    event_starttime = models.DateTimeField(auto_now_add=True)
+    event_endtime = models.DateTimeField(auto_now_add=True)
+    organizer = models.CharField(max_length=100)
+    tickets_count = models.IntegerField()
+    tickets_price = models.FloatField()
+    available_tickets = models.IntegerField()
+    venue_id = models.ForeignKey(Venue, on_delete=models.SET_NULL, null=True)
+
+    def __str__(self):
+        return self.event_name
+    
+class Ticket(models.Model):
+    STATUS_CHOICES = [
+        ('pending', 'Pending'),
+        ('confirmed', 'Confirmed'),
+        ('canceled', 'Canceled')
+    ]
+
+    ticket_id = models.AutoField(primary_key=True)
+    event = models.ForeignKey(Event, on_delete=models.CASCADE)
+    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
+    purchase_date = models.DateTimeField(auto_now_add=True)
+    quantity = models.IntegerField()
+    total_price = models.DecimalField(max_digits=10, decimal_places=2)
+    status = models.CharField(max_length=10, choices=STATUS_CHOICES)
+
+    def __str__(self):
+        return f"Ticket {self.ticket_id} - {self.event.event_name}"
+        
+class Booking(models.Model):
+    booking_id = models.AutoField(primary_key=True)
+    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
+    event_id = models.ForeignKey(Event, on_delete=models.CASCADE)
+    booking_date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Booking {self.booking_id} - {self.user.username}"
+    
+class Payment(models.Model):
+
+    VERIFICATION_CHOICES = [
+        ('pending', 'Pending'),
+        ('verified', 'Verified'),
+        ('rejected', 'Rehected')
+    ]
+    
+    payment_id = models.AutoField(primary_key=True)
+    booking = models.ForeignKey(Booking, on_delete=models.CASCADE, null=True, db_column='Booking_ID')
+    payment_method = models.CharField(max_length=50)
+    amount = models.DecimalField(max_digits=10, decimal_places=2)
+    payment_status = models.CharField(max_length=50)
+    verification_status = models.CharField(max_length=20, choices=VERIFICATION_CHOICES, default='Pending')
+    verification_date = models.DateTimeField(null=True, blank=True)
+    verified_by = models.ForeignKey(CustomUser, on_delete=models.SET_NULL, null=True, blank=True, related_name='verified_payments')
+    verification_notes = models.TextField(blank=True)
+    payment_date = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        return f"Payment {self.payment_id} - {self.amount}"
+    
+class Attendance(models.Model):
+    CHOICES = [
+        ('present', 'Present'),
+        ('absent', 'Absent')
+    ]
+
+    attendance_id = models.AutoField(primary_key=True)
+    user_id = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
+    date = models.DateField()
+    status = models.CharField(max_length=10, choices=CHOICES)
+    created_at = models.DateTimeField(auto_created=True)
+    updated_at = models.DateTimeField(auto_now_add=True)
+
+
+    
